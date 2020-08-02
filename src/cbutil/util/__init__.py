cccc3667e79fcc620e6e55b093865ed56e3d23d7
unknown <bys562bai@gmail.com>
2020-08-02 20:44:57 +0800

    0

--- src/cbutil/util/__init__.py
+++ src/cbutil/util/__init__.py
@@ -4,7 +4,6 @@ from .util import *
 from .url import *
 from .pbar import *
 from .printutil import *
-from .moduleutil import *
 
 import re
 __all__ = list(filter(lambda s: not(re.match('_+',s) or re.search('_+$',s)), globals() ))
